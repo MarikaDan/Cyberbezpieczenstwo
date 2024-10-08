928407ab9c4b4ee94fb3b8a20fa19aafd0a3458d
MarikaDan <126700019+MarikaDan@users.noreply.github.com>
2024-10-08 18:40:33 +0200

    Start projektu

---
+++ LAB1/LAB1/Data/UsersContext.cs
@@ -0,0 +1,11 @@
+﻿using LAB1.Models;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LAB1.Data
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
